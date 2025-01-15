93a78165cf24329c79c1be21d1ff446d5196e45e
胡 <huxinhai807016@gmail.com>
2025-01-15 11:28:55 +0800

    新增http模块

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,7 @@
 export * from './authenticate';
 export * from './getUserInfo';
-export * from './global';
 export * from './sendVerifyCode';
+export * from './global';
+export * from './http/http';
+export * from './http/request';
 //# sourceMappingURL=index.d.ts.map
