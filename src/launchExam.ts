8f2172f9601d45916c73d7a5e5ae760ef9d900ac
leondelee <leevecent@outlook.com>
2025-02-06 15:07:50 +0800

    change

--- src/launchExam.ts
+++ src/launchExam.ts
@@ -18,14 +18,6 @@ export interface ExamRecord {
     message: string // 消息，这里假设是一个JSON字符串
 }
 
-/**
- * 启动考试
- *
- * @function launchExam
- * @param {string} data - 用户 ID
- * @param {string} examType - 考试类型
- * @returns {Promise<ApiResponse<ExamRecord>>} - 返回考试记录的响应结果
- */
-export const launchExam = (data: userId, examType: string): Promise<ApiResponse<ExamRecord>> =>
-    post<ApiResponse<ExamRecord>>('/userService/launchExam', {data, examType})
+export const launchExam = (data :userId & {examType:string}): Promise<ApiResponse<ExamRecord>> =>
+    post<ApiResponse<ExamRecord>>('/userService/launchExam', data)
 
