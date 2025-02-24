32c2e2b7a7a396d4634ebf99e83cd43778f6035d
胡 <huxinhai807016@gmail.com>
2025-02-24 15:29:21 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,18 @@
-<<<<<<< HEAD
+export * from './addNewUser';
 export * from './activate';
-export * from './addNewUser';
 export * from './addQuestion';
-export * from './authenticate';
-=======
-export * from './addNewUser';
->>>>>>> 68972f305bc2095c7afa2090718db5013857d289
 export * from './alertUsers';
 export * from './authenticate';
-export * from './activate';
 export * from './examServerStatus';
-<<<<<<< HEAD
-export * from './getAllUserList';
-export * from './getAllOccupationInfo';
-export * from './getAppReviewStatus';
-export * from './getAPPHotUpdateInfo';
-export * from './getAPPVersion';
-export * from './getCodeLangInfo';
-export * from './getDiscountInfoV2';
-=======
-export * from './getAPPHotUpdateInfo';
-export * from './getAPPVersion';
-export * from './getAllOccupationInfo';
-export * from './getAllUserList';
-export * from './getAppReviewStatus';
-export * from './getCodeLangInfo';
-export * from './getDiscountInfoV2';
 export * from './closeAccount';
->>>>>>> 68972f305bc2095c7afa2090718db5013857d289
+export * from './getAllOccupationInfo';
+export * from './getAllUserList';
+export * from './getAppReviewStatus';
+export * from './getAPPHotUpdateInfo';
+export * from './getAPPVersion';
+export * from './getCodeLangInfo';
+export * from './getDiscountInfoV2';
 export * from './getDistributeInfoV1';
-export * from './addQuestion';
 export * from './getExamDetail';
 export * from './getExamList';
 export * from './getHpLunboPics';
@@ -41,32 +24,21 @@ export * from './getPQList';
 export * from './getProfitInfo';
 export * from './getPublicQuestion';
 export * from './getShareInfo';
-export * from './getSupportStrategies';
-<<<<<<< HEAD
 export * from './getSignal';
+export * from './getSupportStrategies';
 export * from './getSupportTimeFrame';
-export * from './getUserComments';
 export * from './getTokenLatestInfo';
-=======
-export * from './getTokenLatestInfo';
-export * from './getSupportTimeFrame';
-export * from './getUserComments';
->>>>>>> 68972f305bc2095c7afa2090718db5013857d289
 export * from './getUserGoodComments';
+export * from './getUserComments';
 export * from './getUserInfo';
 export * from './global';
-<<<<<<< HEAD
 export * from './getUserPayAvatarInfo';
-export * from './queryOrderInfo';
-export * from './launchInterview';
 export * from './launchExam';
-=======
-export * from './launchExam';
-export * from './queryOrderInfo';
 export * from './launchInterview';
->>>>>>> 68972f305bc2095c7afa2090718db5013857d289
-export * from './queryQuestion';
+export * from './queryOrderInfo';
 export * from './recharge';
+export * from './queryQuestion';
+export * from './rechargePackage';
 export * from './sendLoginInfo';
 export * from './sendVerifyCode';
 export * from './setPageViewRecord';
@@ -74,6 +46,5 @@ export * from './updateExamDetail';
 export * from './updateUserInfo';
 export * from './upsertAlertUser';
 export * from './utdLog';
-export * from './rechargePackage';
 export * from './http/http';
 export * from './http/request';
